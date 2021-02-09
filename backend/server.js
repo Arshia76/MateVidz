787433d02a93e1cc1463b6d9676ee9a76e49fddf
unknown <arshia_nazarlotfi1376@yahoo.com>
2021-02-09 04:24:49 +0330

    making changes

--- backend/server.js
+++ backend/server.js
@@ -77,6 +77,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, console.log(`server started on port ${PORT}`));
