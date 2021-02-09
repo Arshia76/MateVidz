7497e283b0e0d2c65e81b15a0075db4e3f9499cf
unknown <arshia_nazarlotfi1376@yahoo.com>
2021-02-09 04:54:10 +0330

    changing server

--- backend/server.js
+++ backend/server.js
@@ -69,7 +69,7 @@ app.use('/api/search', require('./routes/search'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
   app.get('*', (req, res) => {
-    res.sendFile(__dirname, 'frontend', 'build', 'index.html');
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => {
