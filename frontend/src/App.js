2ac7ccf5753b03b15f28c71b55d9ed140af570f2
unknown <arshia_nazarlotfi1376@yahoo.com>
2021-04-03 12:26:24 +0430

    adding scroll to top

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, Fragment } from 'react';
 import PrivateRoute from './Utils/PrivateRoute';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch, Router } from 'react-router-dom';
 import Header from './Components/Header';
 import Home from './Pages/Home';
 import Register from './Pages/Register';
@@ -13,6 +13,13 @@ import Detail from './Pages/Detail';
 import Chat from './Pages/Chat';
 import Join from './Pages/Join';
 import NotFound from './Pages/NotFound';
+import createHistory from 'history/createBrowserHistory';
+
+export const history = createHistory();
+
+history.listen((location, action) => {
+  window.scrollTo(0, 0);
+});
 
 function App() {
   const dispatch = useDispatch();
@@ -25,7 +32,7 @@ function App() {
   }, [dispatch, user]);
   return (
     <Fragment>
-      <Router>
+      <Router history={history}>
         <Header />
         <Switch>
           <PrivateRoute exact path='/' component={Home} />
